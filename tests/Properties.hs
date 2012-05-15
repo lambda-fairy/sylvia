8bf68915a530fb9b0a70be0dbd15e6a727707b46
Chris Wong <lambda.fairy@gmail.com>
2012-05-15 14:04:48 +1200

    Added tests

---
+++ tests/Properties.hs
@@ -0,0 +1,64 @@
+import Control.Applicative
+import Control.Monad
+import Data.Maybe ( isJust )
+import Data.Void ( Void )
+
+import Test.Framework
+import Test.Framework.Providers.SmallCheck
+import Test.SmallCheck
+import Test.SmallCheck.Series
+
+import Sylvia.Model
+import Sylvia.Text.Parser
+import Sylvia.Text.PrettyPrint
+
+instance Serial Void where
+    series _ = []
+    coseries _ _ = []
+
+instance Serial a => Serial (Inc a) where
+    series = cons0 O \/ cons1IgnoreDepth S
+      where
+        cons1IgnoreDepth c d = [c z | z <- series d]
+
+    coseries rs d = [ \i -> case i of
+                        O   -> z
+                        S x -> f x
+                    | z <- alts0 rs d
+                    , f <- alts1 rs d
+                    ]
+
+instance Serial a => Serial (Exp a) where
+    series = cons1 Ref \/ cons1 Lam \/ cons2 App
+    coseries rs d = [ \e -> case e of
+                        Ref x   -> f x
+                        Lam e'  -> g e'
+                        App a b -> h a b
+                    | f <- alts1 rs d
+                    , g <- alts1 rs d
+                    , h <- alts2 rs d
+                    ]
+
+-- | @parse . pprint == id@
+prop_roundtrip :: Exp Void -> Bool
+prop_roundtrip orig = isJust $ do
+    new <- eitherToMaybe . parseExp . pprintExp $ orig
+    guard $ orig == new
+
+eitherToMaybe :: Either l r -> Maybe r
+eitherToMaybe = either (const Nothing) Just
+
+main :: IO ()
+main = defaultMain $ map (plusTestOptions options)
+    [ testProperty "roundtrip" prop_roundtrip
+    ]
+
+options :: TestOptions
+options = TestOptions
+    { topt_maximum_test_depth = Just 5
+    , topt_timeout = Nothing
+    , topt_seed = Nothing
+    , topt_maximum_generated_tests = Nothing
+    , topt_maximum_unsuitable_generated_tests = Nothing
+    , topt_maximum_test_size = Nothing
+    }
