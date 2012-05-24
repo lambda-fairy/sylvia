e8eb82aaf42806bf8eadfa183e30c521c1397c08
Chris Wong <lambda.fairy@gmail.com>
2012-05-24 14:22:49 +1200

    Print consecutive lambda symbols without parens

--- Sylvia/Text/PrettyPrint.hs
+++ Sylvia/Text/PrettyPrint.hs
@@ -22,12 +22,18 @@ pprintExp = ($ "") . pprint . vacuous
 pprint :: Exp Integer -> ShowS
 pprint e = case e of
     Ref a   -> shows a
-    Lam e'  -> str "(\\ " . pprint (flatten e') . str ")"
+    Lam e'  -> str "(\\" . stackLams (flatten e') . str ")"
     App a b -> pprint a . str " " . case b of
         -- The right term only needs to be bracketed if it contains a
         -- function application
         App _ _ -> str "(" . pprint b . str ")"
         _       -> pprint b
+
+-- | Allow consecutive lambda symbols without adding extra brackets.
+stackLams :: Exp Integer -> ShowS
+stackLams e = case e of
+    Lam e' -> str "\\" . stackLams (flatten e')
+    _      -> str " "  . pprint e
 
 flatten :: Exp (Inc Integer) -> Exp Integer
 flatten = fmap shiftDown
