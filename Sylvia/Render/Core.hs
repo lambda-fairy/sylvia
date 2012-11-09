8c34f8f6e48a2887c6674e048ea73c971acb1657
Chris Wong <lambda.fairy@gmail.com>
2012-11-09 17:58:13 +1300

    Fully switch over to lenses; split big functions into smaller chunks

--- Sylvia/Render/Core.hs
+++ Sylvia/Render/Core.hs
@@ -7,35 +7,12 @@
 -- Maintainer  : chrisyco@gmail.com
 -- Portability : portable
 --
--- This module provides the algorithm used to render expressions. It can
--- be called in two ways:
---
--- 1. A function, 'render', that uses "Sylvia.Render.Backend" to draw a
---    pretty picture;
---
--- 2. Another function, 'render'', that spews its internals all over the
---    place.
+-- This module provides the algorithm used to render expressions.
 
 module Sylvia.Render.Core
     (
     -- * A function
       render
-
-    -- * Another function
-    , render'
-
-    -- ** The result type
-    , Result()
-    , resultImage
-    , resultSize
-    , resultRhyme
-    , resultThroatY
-
-    -- ** Internal bits and pieces
-    , Rhyme
-    , RhymeUnit()
-    , ruIndex
-    , ruDest
     ) where
 
 import Control.Applicative
@@ -65,14 +42,14 @@ $(makeLens ''RhymeUnit)
 
 -- | The result of a rendering operation.
 data Result r = Result
-    { _resultImage :: r
+    { _image :: r
       -- ^ The rendered image.
-    , _resultSize  :: PInt
+    , _size  :: PInt
       -- ^ The size of the image's bounding box in grid units, when all
       -- round things are removed.
-    , _resultRhyme :: Rhyme
+    , _rhyme :: Rhyme
       -- ^ The expression's rhyme.
-    , _resultThroatY :: Int
+    , _midY :: Int
       -- ^ The Y offset of the expression's ear and throat, measured
       -- from the /bottom/ of its bounding box.
     }
@@ -83,11 +60,11 @@ $(makeLens ''Result)
 -- | Render an expression, returning an image along with its size.
 render :: Backend r => Exp Void -> (r, PInt)
 render e =
-    let Result image size rhyme _ = render' $ vacuous e
-    in case rhyme of
-        [] -> (image, size)
+    let Result image' size' rhyme' _ = render' $ vacuous e
+    in case rhyme' of
+        [] -> (image', size')
         _  -> error $ "render: the impossible happened -- "
-                        ++ "extra free variables: " ++ show rhyme
+                        ++ "extra free variables: " ++ show rhyme'
 
 -- | Render an expression, with extra juicy options.
 render' :: Backend r => Exp Integer -> Result r
@@ -98,130 +75,141 @@ render' e = case e of
         Lam _ -> renderBeside a b
         _     -> renderAtop a b
 
+-- | Render two expressions next to each other.
 renderBeside :: Backend r => Exp Integer -> Exp Integer -> Result r
-renderBeside a b = Result image size rhyme aThroatY
+renderBeside a b = Result image' size' rhyme' aMidY
   where
-    image = mconcat $
-        [ aImage
-        , bImage
-        ]
-    (Result aImage (aWidth :| aHeight) aRhyme aThroatY,
-     Result bImage (bWidth :| bHeight) bRhyme bThroatY)
-        = alignThroats
+    image' = aImage <> bImage
+    (Result aImage (aWidth :| aHeight) aRhyme aMidY,
+     Result bImage (bWidth :| bHeight) bRhyme _)
+        = alignMiddle
             (render' a)
-            (shiftX (-aWidth) $ renderWithThroatLine False 1 b)
-    size = (aWidth + bWidth :| max aHeight bHeight)
-    rhyme = aRhyme ++ bRhyme
+            (shiftX (-aWidth) . extendThroatLine (1 :| 0) $ render' b)
+    size' = (aWidth + bWidth :| max aHeight bHeight)
+    rhyme' = bRhyme -- TODO
 
-alignThroats :: Backend r => Result r -> Result r -> (Result r, Result r)
-alignThroats
-    a@(Result _ _ _ aThroatY)
-    b@(Result _ _ _ bThroatY)
-    = (shiftY' (minThroatY - aThroatY) a,
-       shiftY' (minThroatY - bThroatY) b)
+-- | Align two expressions so that their ears and throats are at the same
+-- height.
+alignMiddle :: Backend r => Result r -> Result r -> (Result r, Result r)
+alignMiddle a b
+    = (shiftY' (highest - a^.midY) a,
+       shiftY' (highest - b^.midY) b)
   where
-    minThroatY = min aThroatY bThroatY
+    highest = min (a^.midY) (b^.midY)
 
+-- | Render the first expression above the second, connected with an
+-- applicator dot.
 renderAtop :: Backend r => Exp Integer -> Exp Integer -> Result r
-renderAtop a b = Result image size rhyme bThroatY
+renderAtop a b = Result image' size' rhyme' bMidY
   where
-    image = mconcat $
+    image' = mconcat $
         -- Draw the two sub-expressions
         [ aImage
         , bImage
-        -- Extend the lower sub-expression so it matches up with the
-        -- bigger one
+        -- Extend the lower sub-expression's rhyme lines so it matches up
+        -- with the one above
         , relativeTo (-bWidth :| 0)
             $ extendAcross (bWidth - aWidth) bRhyme
         -- Connect them with a vertical line
-        , drawLine (0 :| aThroatY) (0 :| bThroatY)
+        , drawLine (0 :| aMidY) (0 :| bMidY)
         -- Application dot
-        , drawDot (0 :| bThroatY)
+        , drawDot (0 :| bMidY)
         ]
-    Result aImage (aWidth :| aHeight) aRhyme aThroatY
-        = shiftY (-1 - bHeight) $ renderWithThroatLine False bWidth a
-    Result bImage (bWidth :| bHeight) bRhyme bThroatY
-        = renderWithThroatLine False 1 b
-    size = (aWidth :| aHeight + bHeight + 1)
-    rhyme = aRhyme ++ bRhyme
+    Result aImage (aWidth :| aHeight) aRhyme aMidY
+        = shiftY (-1 - bHeight) . extendThroatLine (bWidth :| 0) $ render' a
+    Result bImage (bWidth :| bHeight) bRhyme bMidY
+        = extendThroatLine (1 :| 0) $ render' b
+    size' = (aWidth :| aHeight + bHeight + 1)
+    rhyme' = aRhyme ++ bRhyme
 
--- | Render an expression with a horizontal line sticking out of its
--- throat. Doesn't sound too comfortable, to be honest.
---
--- The 'resultSize' includes the length of this extra line.
-renderWithThroatLine
+-- | Add a small line sticking out of an expression's throat, moving
+-- the image and increasing the size to compensate.
+extendThroatLine
     :: Backend r
-    => Bool -- ^ Whether the enclosing expression is a lambda.
-    -> Int  -- ^ Length of the throat line. This should be positive.
-    -> Exp Integer -> Result r
-renderWithThroatLine outerIsLam lineLength e = Result image size rhyme throatY
+    => PInt     -- ^ The endpoint of the line, relative to the throat.
+    -> Result r
+    -> Result r
+extendThroatLine (dx :| dy) a
+    = modL image (<> line)
+    . setL midY outerMidY
+    . shiftX' (-dx)
+    $ a
   where
-    Result innerImage innerSize rhyme innerThroatY = render' e
-    -- Shift the main image to the left, then draw a line next to it
-    image = relativeTo (-lineLength :| 0) innerImage <> throatLine
-    throatLine = drawZigzag (-lineLength :| innerThroatY) (0 :| throatY)
-    throatY = if outerIsLam && containsLam e then innerThroatY - 1 else innerThroatY
-    size = innerSize |+| (lineLength :| 0)
+    line = drawZigzag (-dx :| a^.midY) (0 :| outerMidY)
+    outerMidY = a^.midY + dy
+
+-- | Render a lambda expression.
+renderLambda :: Backend r => Exp (Inc Integer) -> Result r
+renderLambda = renderLambda' . fmap shiftDown
+  where
+    renderLambda' e = enclose . extendRhyme . addLine $ render' e
+      where
+        addLine = extendThroatLine . (1 :|) $ if containsLam e then (-1) else 0
 
 -- | Return whether there is a nested box touching the bottom edge of the
 -- bounding box. If True, it means that everything in the image should
--- be shifted down one unit, making the ear and throat lines zigzag.
+-- be shifted down one unit, making the throat line zigzag.
 containsLam :: Exp a -> Bool
 containsLam e = case e of
     Ref _ -> False
     Lam _ -> True
     App _ b -> containsLam b
 
--- | Render a lambda expression.
-renderLambda :: Backend r => Exp (Inc Integer) -> Result r
-renderLambda e' = Result image size rhyme throatY
+-- | Put an expression in a box.
+enclose :: Backend r => Result r -> Result r
+enclose = enclose' . shiftY (-1)
   where
-    Result innerImage (innerWidth :| innerHeight) innerRhyme throatY
-        = shiftY (-1) . renderWithThroatLine True 1 $ fmap shiftDown e'
-    -- Draw a box around it
-    image = drawBox (negateP size) size throatY
-            <> relativeTo (-width :| 0) rhymeImage
-            <> innerImage
-    (rhymeImage, rhyme) = renderRhyme throatY innerRhyme
-    rhymeHeight = fromInteger . maximumOr 0 $ map (getL ruIndex) innerRhyme
-    size@(width :| _) = (innerWidth + 1 :| (max innerHeight rhymeHeight) + 2)
-
--- | Like 'maximum', but returns a default value on an empty list rather
--- than throwing a hissy fit.
-maximumOr :: Ord a => a -> [a] -> a
-maximumOr def = foldl' max def
+    enclose' a
+        = modL image (<> drawBox (negateP boxSize) boxSize (a^.midY))
+        . setL size  boxSize
+        $ a
+      where
+        boxSize = a^.size |+| (0 :| 2)
 
 -- | Render an expression's rhyme.
-renderRhyme
-    :: Backend r
-    => Int        -- ^ Throat offset (see 'resultThroatY')
-    -> Rhyme      -- ^ The inner expression's rhyme
-    -> (r, Rhyme) -- ^ The resulting image, along with the outer rhyme
-renderRhyme throatY innerRhyme = (foldMap renderOne innerRhyme, outerRhyme)
+extendRhyme :: Backend r => Result r -> Result r
+extendRhyme a
+    = modL image (<> rhymeImage)
+    . modL rhyme flattenRhyme
+    . modL size  ((fstP ^+= 1) . (sndP ^%= max rhymeHeight))
+    $ a
   where
-    renderOne (RhymeUnit index dest) = drawLine (0 :| throatY - fromInteger index) (1 :| dest)
-    outerRhyme =
-        [ RhymeUnit (pred index) (throatY - fromInteger index)
-        | RhymeUnit index _ <- innerRhyme
-        , index > 0
-        ]
+    rhymeHeight = fromInteger . maximum0 $ map (^.ruIndex) (a^.rhyme)
+    rhymeImage = relativeTo (-(a^.size^.fstP) :| 0) $ foldMap drawRhymeUnit (a^.rhyme)
+      where drawRhymeUnit (RhymeUnit index dest)
+                = drawLine (-1 :| makeOffset index) (0 :| dest)
+    flattenRhyme inner =
+        [ RhymeUnit (pred index) (makeOffset index)
+        | RhymeUnit index _ <- inner, index > 0 ]
+    makeOffset = (a^.midY -) . fromInteger
+
+-- | Like 'maximum', but returns a zero on an empty list rather than
+-- throwing a hissy fit.
+maximum0 :: (Num a, Ord a) => [a] -> a
+maximum0 = foldl' max 0
 
 -- | Shift an image horizontally by a specified amount.
 shiftX :: Backend r => Int -> Result r -> Result r
-shiftX dx = modL resultImage (relativeTo (dx :| 0))
+shiftX dx = modL image (relativeTo (dx :| 0))
+
+-- | Like 'shiftX', but expand the image's bounding box rather than shifting it.
+shiftX' :: Backend r => Int -> Result r -> Result r
+shiftX' dx
+    = modL size (fstP ^+= abs dx)
+    . shiftX dx
 
 -- | Shift an image vertically by a specified amount, changing the rhyme
 -- and throat position to compensate.
 shiftY :: Backend r => Int -> Result r -> Result r
 shiftY dy
-    = modL resultImage   (relativeTo (0 :| dy))
-    . modL resultRhyme   (map (modL ruDest (+ dy)))
-    . modL resultThroatY (+ dy)
+    = modL image (relativeTo (0 :| dy))
+    . modL rhyme (map (ruDest ^+= dy))
+    . modL midY  (+ dy)
 
 -- | Like 'shiftY', but expand the image's bounding box rather than shifting it.
 shiftY' :: Backend r => Int -> Result r -> Result r
 shiftY' dy
-    = modL resultSize (modL sndP (subtract dy)) -- dy is usually negative, so this /increases/ the size
+    = modL size (sndP ^+= abs dy)
     . shiftY dy
 
 extendAcross :: Backend r => Int -> Rhyme -> r
